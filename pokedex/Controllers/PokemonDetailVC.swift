9d5575415de0640906a58697d7c6476959605af2
Rom Sharony <romsharony@Roms-MacBook-Pro.local>
2017-06-30 14:41:23 +0300

    finished static detailes page

--- pokedex/Controllers/PokemonDetailVC.swift
+++ pokedex/Controllers/PokemonDetailVC.swift
@@ -13,7 +13,20 @@ class PokemonDetailVC: UIViewController {
     var pokemon: Pokemon!
     
     @IBOutlet weak var nameLbl: UILabel!
-
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var idLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,5 +35,9 @@ class PokemonDetailVC: UIViewController {
     
     
 
-
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
--- pokedex/Models/pokemon.swift
+++ pokedex/Models/pokemon.swift
@@ -12,30 +12,62 @@ class Pokemon {
     
     private var _name: String!
     private var _id: Int!
+    private var _description: String!
+    private var _type: String!
+    private var _defense: String!
+    private var _attack: String!
+    private var _height: String!
+    private var _weigth: String!
+    private var _nextEvoText: String!
+    
+    init(name: String, id: Int, description: String, type: String,
+         defense: String, attack: String, height: String,
+         weight: String, nextEvoText: String) {
+        self._name = name
+        self._id = id
+        self._description = description
+        self._type = type
+        self._defense = defense
+        self._attack = attack
+        self._height = height
+        self._weigth = weight
+        self._nextEvoText = nextEvoText
+    }
     
     var name: String {
-        get {
-            return _name
-        }
-        set (newName) {
-            _name = newName
-        }
+        return _name
     }
     
     var id: Int {
-        get {
-            return _id
-        }
-        set (newId) {
-            _id = newId
-        }
+        return _id
     }
     
-    init(name: String, id: Int) {
-        self._name = name
-        self._id = id
+    var description: String {
+        return _description
     }
     
+    var type: String {
+        return _type
+    }
     
+    var defense: String {
+        return _defense
+    }
+    
+    var attack: String {
+        return _attack
+    }
+    
+    var height: String {
+        return _height
+    }
+    
+    var weigth: String {
+        return _weigth
+    }
+    
+    var nextEvoText: String {
+        return _nextEvoText
+    }
     
 }
